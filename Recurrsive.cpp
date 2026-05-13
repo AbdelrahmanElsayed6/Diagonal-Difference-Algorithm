5dc81cb409dc4e8b1fa063e3b7b1f55448cba745
Abdelrahman <heroabdelrahmanelsayed@gmail.com>
2026-05-13 16:57:36 +0300

    Refactor recursive functions to use C-style arrays

--- Recurrsive.cpp
+++ Recurrsive.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
-int calculateRecursive(const std::vector<std::vector<int>>& matrix, int n, int i) {
-    if (i == n) return 0; 
+
+// Recurrsive :
+int calculateRecursive(int matrix[][100], int n, int i) {
+    if (i == n) return 0;
 
     int rowDiff = matrix[i][i] - matrix[i][n - 1 - i];
     return rowDiff + calculateRecursive(matrix, n, i + 1);
 }
 
-int diagonalDifferenceRecursive(const std::vector<std::vector<int>>& matrix, int n) {
-    return std::abs(calculateRecursive(matrix, n, 0));
+int diagonalDifferenceRecursive(int matrix[][100], int n) {
+    return abs(calculateRecursive(matrix, n, 0));
 }
+
